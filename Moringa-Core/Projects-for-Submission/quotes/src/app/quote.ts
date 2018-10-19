567fdd3ee72268d770e6e1256e0f9329ab1bb048
Jeremy Kimotho <jeremykimotho@yahoo.com>
2018-10-19 16:14:08 +0300

    I have added part of the upvote and downvote functionality

--- Moringa-Core/Projects-for-Submission/quotes/src/app/quote.ts
+++ Moringa-Core/Projects-for-Submission/quotes/src/app/quote.ts
@@ -1,6 +1,6 @@
 export class Quote {
   public showDetails:boolean;
-  constructor(public sentence: string, public author: string, public submitter:string, public submissionDate: Date) {
+  constructor(public sentence: string, public author: string, public submitter:string, public submissionDate: Date, public upvotes:[number], public downvotes:[number]) {
     this.showDetails = true
   }
 }
