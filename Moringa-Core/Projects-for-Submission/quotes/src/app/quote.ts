6f13a677af7299f99f2e7403daf9584a4043d6f2
Jeremy Kimotho <jeremykimotho@yahoo.com>
2018-10-20 16:16:59 +0300

    i have finished adding bootstrap styling and started making the custom stylesheets

--- Moringa-Core/Projects-for-Submission/quotes/src/app/quote.ts
+++ Moringa-Core/Projects-for-Submission/quotes/src/app/quote.ts
@@ -1,7 +1,7 @@
 export class Quote {
   public showDetails:boolean;
   constructor(public sentence: string, public author: string, public submitter:string, public submissionDate: string, public upvotes:Array<number>, public downvotes:Array<number>){
-    this.showDetails = true
+    this.showDetails = false
   }
 }
 
